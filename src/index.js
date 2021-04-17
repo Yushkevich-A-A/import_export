55408001b5463a30b32106960540e29b340286d5
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-04-17 22:44:57 +0300

    files domaine.js game.js created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import './css/style.css';
+import './css/styles.css';
 
-import './js/app.js'
+import './js/app.js';
---
+++ src/js/game.js
@@ -0,0 +1,16 @@
+import Character from './domaine.js'
+
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+export  class GameSavingData {
+  }
+  
+export  function readGameSaving() {
+  }
+  
+export  function writeGameSaving() {
+  }
